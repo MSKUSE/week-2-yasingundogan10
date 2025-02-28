20980ebe3473275a9874961150105b6a8269487c
yasingundogan10 <yasingundogan@posta.mu.edu.tr>
2025-02-28 12:07:35 +0300

    week 2

---
+++ src/Dt.java
@@ -0,0 +1,16 @@
+public class Dt {
+    public static void main(String[] args) {
+        int grade = 1;
+        double grade2  = 1.5 ;
+        boolean isOKay = true ;
+        char first = 'A';
+
+        System.out.println(grade);
+        System.out.println(grade2);
+        System.out.println(isOKay);
+        System.out.println(first);
+
+
+
+    }
+}
